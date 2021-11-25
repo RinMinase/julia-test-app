ba1a6fa411070578c15164b45c3500c2935a7a90
Rin Minase <rin.black.raison.detre@gmail.com>
2021-11-26 01:07:03 +0800

    Added square of sums

--- src/modules/6_differences-of-squares.jl
+++ src/modules/6_differences-of-squares.jl
@@ -1,5 +1,19 @@
 export differences_of_squares
 
-function differences_of_squares()
+function differences_of_squares(input::Integer)
+  sum = 0
+  sumOfSquares = 0
 
+  for i in 1:input
+    sum += i
+    sumOfSquares += i ^ 2
+  end
+
+  squareOfSums = sum ^ 2
+  difference = squareOfSums - sumOfSquares
+
+  println("Square of sums: " * string(squareOfSums))
+  println("Sums of Squares: " * string(sumOfSquares))
+  println("===========================")
+  println("Difference: " * string(difference))
 end
