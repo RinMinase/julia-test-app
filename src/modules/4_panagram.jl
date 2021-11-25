b9ab78ee3e7b5de17abdbe355bf91608a65ff51d
Rin Minase <rin.black.raison.detre@gmail.com>
2021-11-26 00:58:47 +0800

    Added panagram and rotational cipher

--- src/modules/4_panagram.jl
+++ src/modules/4_panagram.jl
@@ -1,5 +1,12 @@
 export panagram
 
-function panagram()
-  print("tests");
+function panagram(input::String)
+  alphabet = Set('a':'z')
+  inputLower = Set(lowercase(input))
+
+  if issubset(alphabet, inputLower)
+    print("panagram")
+  else
+    print("not a panagram")
+  end
 end
--- src/modules/5_rotational-cipher.jl
+++ src/modules/5_rotational-cipher.jl
@@ -1,5 +1,27 @@
 export rotational_cipher
 
-function rotational_cipher()
+function rotational_cipher(key::Integer, input::String)
+  println("Plain: " * input)
 
+  if key % 26 == 0
+    println("Cipher: " * input)
+  else
+    println("Cipher: " * rotate(key, input))
+  end
+end
+
+function rotate(rotations, input)::String
+  output = []
+
+  for i in input
+    if i in 'a':'z'
+      push!(output, i + rotations)
+    elseif i in 'A':'Z'
+      push!(output, i + rotations)
+    else
+      push!(output, i)
+    end
+  end
+
+  return join(output)
 end
