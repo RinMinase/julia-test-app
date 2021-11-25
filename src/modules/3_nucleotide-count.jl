ae0ce277136e2f6addec0448d21bdc0b55d2ee0b
Rin Minase <rin.black.raison.detre@gmail.com>
2021-11-26 00:14:02 +0800

    Appended nucleotide counter

--- src/modules/3_nucleotide-count.jl
+++ src/modules/3_nucleotide-count.jl
@@ -1,5 +1,38 @@
 export nucleotide_count
 
-function nucleotide_count()
+function nucleotide_count(input::String)
+  if isValidNucleotide(input)
+    enumerateNucleotides(input)
+  else
+    print("error")
+  end
+end
 
+function isValidNucleotide(input)
+  for i in input
+    if i != 'A' && i != 'C' && i != 'G' && i != 'T'
+      return false
+    end
+  end
+
+  return true
+end
+
+function enumerateNucleotides(input)
+  A = 0
+  C = 0
+  G = 0
+  T = 0
+
+  for i in input
+    if i == 'A' A += 1 end
+    if i == 'C' C += 1 end
+    if i == 'G' G += 1 end
+    if i == 'T' T += 1 end
+  end
+
+  print("\'A\': " * string(A) * ", ")
+  print("\'C\': " * string(C) * ", ")
+  print("\'G\': " * string(G) * ", ")
+  println("\'T\': " * string(T))
 end
