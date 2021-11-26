fe53c3db56c91b9eb98f76dad94cb64673e7ed65
Rin Minase <rin.black.raison.detre@gmail.com>
2021-11-26 14:51:32 +0800

    Added raindrops

--- src/modules/7_raindrops.jl
+++ src/modules/7_raindrops.jl
@@ -1,5 +1,15 @@
 export raindrops
 
-function raindrops()
+function raindrops(input::Integer)
+  output = ""
 
+  if input % 3 == 0 (output *= "Pling") end
+  if input % 5 == 0 (output *= "Plang") end
+  if input % 7 == 0 (output *= "Plong") end
+
+  if output == ""
+    print(string(input))
+  else
+    print(output)
+  end
 end
