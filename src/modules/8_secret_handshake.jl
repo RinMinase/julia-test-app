81651e1369d6f06b47b239b91257285607939473
Rin Minase <rin.black.raison.detre@gmail.com>
2021-11-26 17:41:19 +0800

    Added secret handshake

--- src/modules/8_secret_handshake.jl
+++ src/modules/8_secret_handshake.jl
@@ -1,5 +1,24 @@
 export secret_handshake
 
-function secret_handshake()
+function secret_handshake(input::Integer)
+  print(handshake(input))
+end
 
+function handshake(input)::Array{String}
+  output = []
+
+  if (input < 32)
+    binary = digits(input, base=2, pad=5)
+
+    binary[1] == 1 && push!(output, "wink")
+    binary[2] == 1 && push!(output, "double blink")
+    binary[3] == 1 && push!(output, "close your eyes")
+    binary[4] == 1 && push!(output, "jump")
+
+    if (binary[5] == 1)
+      output = reverse(output)
+    end
+  end
+
+  return output
 end
