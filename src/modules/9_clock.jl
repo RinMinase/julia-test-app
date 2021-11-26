f0931a6416811fc2790db38135c33394032540eb
Rin Minase <rin.black.raison.detre@gmail.com>
2021-11-26 17:41:41 +0800

    Added clock

--- src/modules/9_clock.jl
+++ src/modules/9_clock.jl
@@ -1,5 +1,34 @@
-export clock
+export Clock
 
-function clock()
+struct Clock
+  minutes::Integer
 
+  function Clock(hours::Integer, minutes::Integer)
+    totalMinutes = 60 * hours + minutes
+    dailyMinutes = 24 * 60
+
+    while totalMinutes < 0
+      totalMinutes += dailyMinutes
+    end
+
+    return new(totalMinutes % dailyMinutes)
+  end
+
+  function Base.:+(clk::Clock, minute::Integer)
+    return Clock(0, clk.minutes + minute)
+  end
+
+  function Base.:-(clk::Clock, minute::Integer)
+    return Clock(0, clk.minutes - minute)
+  end
+
+  function Base.show(io::IO, clk::Clock)
+    hr = floor(Int, clk.minutes / 60)
+    min = floor(Int, clk.minutes % 60)
+
+    paddedHours = lpad(string(hr), 2, '0')
+    paddedMinutes = lpad(string(min), 2, '0')
+
+    print(io, "$paddedHours:$paddedMinutes")
+  end
 end
