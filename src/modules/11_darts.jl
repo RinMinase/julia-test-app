b82d9e7cbb607ec34ef5b5edcae8de49e05fba09
Rin Minase <rin.black.raison.detre@gmail.com>
2021-12-02 00:23:30 +0800

    Added darts

--- src/modules/11_darts.jl
+++ src/modules/11_darts.jl
@@ -1,5 +1,16 @@
 export darts
 
-function darts()
+function darts(x::Int, y::Int)
+  hyp = hypot(x, y)
+  score = 10
 
+  if hyp > 10
+    score = 0
+  elseif hyp > 5
+    score = 1
+  elseif hyp > 1
+    score = 5
+  end
+
+  println("Score: " * string(score))
 end
