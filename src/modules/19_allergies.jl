e03674354b4a19e0c388abe07af2dca288e1a00a
Rin Minase <rin.black.raison.detre@gmail.com>
2021-12-02 00:24:39 +0800

    Added allergies transcription

--- src/modules/19_allergies.jl
+++ src/modules/19_allergies.jl
@@ -1,5 +1,30 @@
 export allergies
 
-function allergies()
+function allergies(score::Int)
+  allergyList = Dict(
+    1 => "eggs",
+    2 => "peanuts",
+    4 => "shellfish",
+    8 => "strawberries",
+    16 => "tomatoes",
+    32 => "chocolate",
+    64 => "pollen",
+    128 => "cats",
+  )
 
+  scoreAllergies = []
+
+  for (value, allergen) in pairs(allergyList)
+    if (score & value) != 0
+      push!(scoreAllergies, allergen)
+    end
+  end
+
+  print("Allergens: ")
+
+  for allergen in scoreAllergies
+    print("\"$allergen\" ")
+  end
+
+  println()
 end
