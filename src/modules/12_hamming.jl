4c821cc0ec16bae784b9c8bbaf4cf9bbe12bdb73
Rin Minase <rin.black.raison.detre@gmail.com>
2021-12-02 00:23:40 +0800

    Added hamming counter

--- src/modules/12_hamming.jl
+++ src/modules/12_hamming.jl
@@ -1,5 +1,23 @@
 export hamming
 
-function hamming()
+function hamming(strand1::String, strand2::String)
+  if length(strand1) != length(strand2)
+    println("Length of strands must be equal")
+  else
+    distance = hamming_count(strand1, strand2)
 
+    println("Hamming distance: " * string(distance))
+  end
+end
+
+function hamming_count(strand1, strand2)
+  difference = 0
+
+  for (char1, char2) in zip(strand1, strand2)
+    if char1 != char2
+      difference += 1
+    end
+  end
+
+  return difference
 end
