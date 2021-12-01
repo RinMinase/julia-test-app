4b99692170571f1e5fedd836d52703a8137a003a
Rin Minase <rin.black.raison.detre@gmail.com>
2021-12-02 00:24:10 +0800

    Added scrabble score

--- src/modules/15_scrabble-score.jl
+++ src/modules/15_scrabble-score.jl
@@ -1,5 +1,33 @@
 export scrabble_score
 
-function scrabble_score()
+function scrabble_score(str::String)
+  if scrabbleValidWord(str)
+    score = 0
 
+    for ch in str
+      score += scrabbleCharPoint(ch)
+    end
+
+    println("Word Score: $score")
+  else
+    println("Invalid word")
+  end
+end
+
+function scrabbleValidWord(str)
+  return !occursin(r"\W", str)
+end
+
+function scrabbleCharPoint(c)
+  ch = uppercase(c)
+
+  ch in "AEIOULNRST" && return 1
+  ch in "DG" && return 2
+  ch in "BCMP" && return 3
+  ch in "FHVWY" && return 4
+  ch in "K" && return 5
+  ch in "JX" && return 8
+  ch in "QZ" && return 10
+
+  return 0
 end
