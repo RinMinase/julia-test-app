31f6fff3cb8808e7f83905a8b86c251a6ee44e68
Rin Minase <rin.black.raison.detre@gmail.com>
2021-12-02 00:23:25 +0800

    Added robot name

--- src/modules/10_robot-name.jl
+++ src/modules/10_robot-name.jl
@@ -1,5 +1,15 @@
 export robot_name
 
-function robot_name()
+oldNames = []
 
+function robot_name()
+  # todo
+end
+
+function randomizer()
+  letter = Random.randstring('A': 'Z', 1)
+  letter2 = Random.randstring('A': 'Z', 1)
+  number = Random.rand(100:999)
+
+  return "$letter$letter2$number"
 end
