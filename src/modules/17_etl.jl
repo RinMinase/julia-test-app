c471ce53f987d427458e77050846f9db6b8a5cd7
Rin Minase <rin.black.raison.detre@gmail.com>
2021-12-02 00:24:22 +0800

    Added ETL

--- src/modules/17_etl.jl
+++ src/modules/17_etl.jl
@@ -1,5 +1,37 @@
 export etl
 
 function etl()
+  # Sample ETL
+  dict = Dict(
+    1 => "AEIOULNRST",
+    2 => "DG",
+    3 => "BCMP",
+    4 => "FHVWY",
+    5 => "K",
+    8 => "JX",
+    10 => "QZ",
+  )
 
+  newDict = etl_process(dict)
+
+  println("Old dictionary\n $dict")
+  println("\nNew dictionary\n $newDict")
+end
+
+function etl(input::Dict)
+  newDict = etl_process(input)
+
+  println(newDict)
+end
+
+function etl_process(input::Dict)
+  output = Dict()
+
+  for (value, letters) in input
+		for ch in letters
+			output[lowercase(ch)] = value
+		end
+	end
+
+  return output
 end
