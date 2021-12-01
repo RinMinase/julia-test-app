c2c7ae32a67a86b23aa20c71d40c4ccbf0fd73a0
Rin Minase <rin.black.raison.detre@gmail.com>
2021-12-02 00:24:17 +0800

    Added word counter

--- src/modules/16_word-count.jl
+++ src/modules/16_word-count.jl
@@ -1,5 +1,20 @@
 export word_count
 
-function word_count()
+function word_count(sentence::String)
+  str = lowercase(sentence)
+  wordRegex = r"\w+('\w)?"
+  wordList = Dict()
 
+  for word in eachmatch(wordRegex, str)
+    if !haskey(wordList, word.match)
+      # if item does not exist on dictionary
+      wordList[word.match] = 1
+    else
+      wordList[word.match] += 1
+    end
+  end
+
+  for (word, iterations) in wordList
+    println("$word: $iterations")
+  end
 end
