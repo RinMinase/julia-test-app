5470d29ccd7ca0fd1c1c78413837b4a8e162254d
Rin Minase <rin.black.raison.detre@gmail.com>
2021-12-02 00:24:47 +0800

    Added isogram

--- src/modules/20_isogram.jl
+++ src/modules/20_isogram.jl
@@ -1,5 +1,12 @@
 export isogram
 
-function isogram()
+function isogram(str::String)
+  str = filter(isletter, lowercase(str))
+  isIsogram = length(unique(str)) == length(str)
 
+  if isIsogram
+    println("Input is an isogram")
+  else
+    println("Not an isogram")
+  end
 end
