65c7684a9b7008c63c92a794dc8bfb53b0f7c0df
Rin Minase <rin.black.raison.detre@gmail.com>
2021-12-02 00:23:48 +0800

    Added RNA transcription

--- src/modules/13_rna-transcription.jl
+++ src/modules/13_rna-transcription.jl
@@ -1,5 +1,35 @@
 export rna_transcription
 
-function rna_transcription()
-
+function rna_transcription(nucleotide::Char)
+  if nucleotide == 'C'
+    println("RNA complement: G")
+  elseif nucleotide == 'G'
+    println("RNA complement: C")
+  elseif nucleotide == 'T'
+    println("RNA complement: A")
+  elseif nucleotide == 'A'
+    println("RNA complement: U")
+  else
+    println("Invalid nucleotide")
+  end
 end
+
+function rna_transcription(strand::String)
+  table = Dict(
+    'C' => 'G',
+    'G' => 'C',
+    'T' => 'A',
+    'A' => 'U',
+  )
+
+  try
+    transcription = map(ch -> table[ch], strand)
+
+    println("Strand complement: " * transcription)
+  catch err
+    if isa(err, KeyError)
+      println("Invalid strand")
+    end
+  end
+end
+
