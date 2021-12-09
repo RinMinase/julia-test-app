b3c390a59f07dd41acdf2fc926132ec117ea9a34
Rin Minase <rin.black.raison.detre@gmail.com>
2021-12-09 16:18:36 +0800

    Added atbash cipher

--- src/modules/24_atbash-cipher.jl
+++ src/modules/24_atbash-cipher.jl
@@ -1,5 +1,19 @@
 export atbash_cipher
+export atbash_cipher_decode
 
-function atbash_cipher()
+function atbash_cipher(input)
+  output = map(y -> atbashCipher(y), input)
+  println(output)
+end
 
+function atbash_cipher_decode(input)
+  output = map(y -> atbashCipher(y), input)
+  println(output)
+end
+
+function atbashCipher(ch::Char)
+  isdigit(ch) && return ch
+  (!isdigit(ch) && !isletter(ch)) && return ch
+
+  return 'z' - lowercase(ch) + 'a'
 end
