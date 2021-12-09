d94c34976b68a6c0efc9fa22f61c786f81f4c04b
Rin Minase <rin.black.raison.detre@gmail.com>
2021-12-09 16:19:41 +0800

    Added sieve

--- src/modules/28_sieve.jl
+++ src/modules/28_sieve.jl
@@ -1,5 +1,27 @@
 export sieve
 
-function sieve()
+function sieve(limit::Int)
+  result = []
+  prime = fill(true, limit)
 
+  for i in 2:limit
+    if prime[i]
+      push!(result, i)
+
+      # starts at 2 * i
+      # increments by i
+      # ends at limit
+      for j in 2 * i : i : limit
+        prime[j] = false
+      end
+    end
+  end
+
+  print("Primes: ")
+
+  for i in result
+    print("$i ")
+  end
+
+  println()
 end
