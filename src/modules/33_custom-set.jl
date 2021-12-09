b1333b30411fac4f58378284a18eead0bee15c4e
Rin Minase <rin.black.raison.detre@gmail.com>
2021-12-09 16:20:23 +0800

    Added custom set

--- src/modules/33_custom-set.jl
+++ src/modules/33_custom-set.jl
@@ -1,5 +1,22 @@
 export custom_set
 
 function custom_set()
+  CustomSet = Set
 
+  disjoint = isdisjoint
+  complement = setdiff
+  complement! = setdiff!
+
+  set1 = CustomSet([1, 2, 3, 4])
+  set2 = CustomSet([])
+
+  print("disjoint: ")
+  println(disjoint(set1, set2))
+
+  set3 = complement(set1, 1:2:6)
+  print("complement: ")
+  println(set3)
+
+  print("complement (mutation): ")
+  println(complement!(set3, 1:3:6))
 end
