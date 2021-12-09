92485e54266b676001556ce958bb91e4de3e3271
Rin Minase <rin.black.raison.detre@gmail.com>
2021-12-09 16:18:43 +0800

    Added grains

--- src/modules/25_grains.jl
+++ src/modules/25_grains.jl
@@ -1,5 +1,16 @@
-export grains
+export grains_on_square
+export grains_total_number
 
-function grains()
+function grains_on_square(square::Int)
+  square <= 0 && println("Invalid number")
+  square > 64 && println("There are only 64 squares on a board")
 
+  println("Grains on Square: " * string(2^(square-1)))
+end
+
+function grains_total_number(square::Int)
+  square <= 0 && println("Invalid number")
+  square > 64 && println("There are only 64 squares on a board")
+
+  println("Total Grains on Board: " * string(2^(square) - 1))
 end
