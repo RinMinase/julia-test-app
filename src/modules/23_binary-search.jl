660574a152ad3b337ae3e2c77da84f94af130cb0
Rin Minase <rin.black.raison.detre@gmail.com>
2021-12-09 16:18:30 +0800

    Added binary search

--- src/modules/23_binary-search.jl
+++ src/modules/23_binary-search.jl
@@ -1,5 +1,31 @@
 export binary_search
 
 function binary_search()
+  # example usage of binary search
+  haystack = [1,3,5,7,9,11]
+  needle = 4
 
+  bst(haystack, needle)
+end
+
+function binary_search(haystack, needle)
+  bst(haystack, needle)
+end
+
+function bst(haystack, needle)
+  low = 1
+  high = length(haystack)
+
+  while low <= high
+    idx = Int(floor((high + low) / 2))
+    if needle > haystack[idx]
+      low = idx + 1
+    elseif needle < haystack[idx]
+      high = idx - 1
+    else
+      return idx:idx
+    end
+  end
+
+  println("Low: $low \nHigh: $high")
 end
