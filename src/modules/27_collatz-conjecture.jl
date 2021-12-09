714f830e453b1a9cdeac420f6b52a28bf810d242
Rin Minase <rin.black.raison.detre@gmail.com>
2021-12-09 16:19:37 +0800

    Added collatz conjecture

--- src/modules/27_collatz-conjecture.jl
+++ src/modules/27_collatz-conjecture.jl
@@ -1,5 +1,16 @@
 export collatz_conjecture
 
-function collatz_conjecture()
+function collatz_conjecture(input::Int)
+  if input < 1
+    println("Number not accepted")
+  else
+    steps = 0
 
+    while input > 1
+      steps += 1
+      input = iseven(Int(input)) ? input / 2 : 3 * input + 1
+    end
+
+    return steps
+  end
 end
