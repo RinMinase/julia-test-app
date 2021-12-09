678e49a34aacac29350aa667ed2742375b48e03a
Rin Minase <rin.black.raison.detre@gmail.com>
2021-12-09 18:57:27 +0800

    Modified differences of squares

--- src/modules/6_differences-of-squares.jl
+++ src/modules/6_differences-of-squares.jl
@@ -1,19 +1,37 @@
 export differences_of_squares
 
+export differenceOfSquares
+export squareOfSums
+export sumsOfSquares
+
 function differences_of_squares(input::Integer)
+  squares = squareOfSums(input)
+  sums = sumsOfSquares(input)
+
+  println("Square of sums: $squares")
+  println("Sums of Squares: $sums")
+  println("===========================")
+  println("Difference: $(differenceOfSquares(input))")
+end
+
+function squareOfSums(input)
   sum = 0
-  sumOfSquares = 0
 
   for i in 1:input
     sum += i
-    sumOfSquares += i ^ 2
   end
 
-  squareOfSums = sum ^ 2
-  difference = squareOfSums - sumOfSquares
-
-  println("Square of sums: " * string(squareOfSums))
-  println("Sums of Squares: " * string(sumOfSquares))
-  println("===========================")
-  println("Difference: " * string(difference))
+  return sum ^ 2
 end
+
+function sumsOfSquares(input)
+  squares = 0
+
+  for i in 1:input
+    squares += i ^ 2
+  end
+
+  return squares
+end
+
+differenceOfSquares(input) = squareOfSums(input) - sumsOfSquares(input)
