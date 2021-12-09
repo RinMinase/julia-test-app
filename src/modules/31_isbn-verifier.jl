5af577fd619221dd5c6d37ab04c7a37d9559f603
Rin Minase <rin.black.raison.detre@gmail.com>
2021-12-09 16:19:59 +0800

    Added isbn verifier

--- src/modules/31_isbn-verifier.jl
+++ src/modules/31_isbn-verifier.jl
@@ -1,5 +1,34 @@
-export isbn_verifier
+export isbn
 
-function isbn_verifier()
+function isbn(input::AbstractString)
+  if isISBNValid(input)
+    println("ISBN is valid")
+  else
+    println("ISBN is invalid")
+  end
+end
 
+function isISBNValid(input)
+  total = 0;
+
+  replaced = replace(input, "-" => "")
+  reversed = reverse(replaced)
+  replaced = tryparse(Int, reversed)
+  normalized = digits(replaced)
+
+  for (index, value) in enumerate(normalized)
+    if value == "X"
+      normalized[index] = 10
+    end
+  end
+
+  for (index, value) in enumerate(normalized)
+    if isnan(value)
+      return false
+    end
+
+    total += normalized[index] * (10 - index + 1)
+  end
+
+  return total % 11 == 0
 end
