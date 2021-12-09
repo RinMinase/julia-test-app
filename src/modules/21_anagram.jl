35a83b066ac57892168c997573f0a0ec2eddab74
Rin Minase <rin.black.raison.detre@gmail.com>
2021-12-09 16:18:01 +0800

    Added anagram

--- src/modules/21_anagram.jl
+++ src/modules/21_anagram.jl
@@ -1,5 +1,25 @@
 export anagram
 
-function anagram()
+function anagram(str::String, candidates::Array)
+  function isanagram(x, y)
+    x = uppercase(x)
+    y = uppercase(y)
 
+    (x != y) && sort(collect(x)) == sort(collect(y))
+  end
+
+  output = []
+  for candidate in candidates
+    if isanagram(candidate, str)
+      push!(output, candidate)
+    end
+  end
+
+  print("Anagrams: ")
+
+  for i in output
+    print("$i ")
+  end
+
+  println()
 end
