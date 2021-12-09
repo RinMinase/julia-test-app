da18a8a52b392997359e0c48bd002bdb9201d3aa
Rin Minase <rin.black.raison.detre@gmail.com>
2021-12-09 16:20:07 +0800

    Added trinary

--- src/modules/32_trinary.jl
+++ src/modules/32_trinary.jl
@@ -1,5 +1,12 @@
 export trinary
 
-function trinary()
+function trinary(input::String)
+  output = 0
 
+  for ch in input
+    ch in "012" || return 0
+    output = (3 * output) + ch - '0'
+  end
+
+  println("Decimal: $output")
 end
